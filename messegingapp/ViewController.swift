1cd85a9066fcf6ad091f4dfa156d49cf6dd887fd
Onkanya <onkanyalearning@gmail.com>
2019-11-18 09:16:09 +0700

    deleteMessage

--- messegingapp/ViewController.swift
+++ messegingapp/ViewController.swift
@@ -19,12 +19,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var postData = [String]()
     
+    var postKey = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         TableView.delegate = self
         TableView.dataSource = self
+        
+        TableView.allowsMultipleSelectionDuringEditing = true
         
         // Set the firebase reference
         ref = Database.database().reference()
@@ -36,14 +40,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             // Try to convert the value of the data to a string
             let post = snapshot.value as? String
+            let key = snapshot.key
             
             if let actualPost = post {
                 
                 // Append the data to our postData array
                 self.postData.append(actualPost)
                 
+                // Append the data key to postData array
+                self.postKey.append(key)
+                
                 // Reload the tableview
                 self.TableView.reloadData()
+                
             }
         })
     }
@@ -58,7 +67,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell!
     }
     
-
-
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            tableView.beginUpdates()
+            
+            // delete row from tableView
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+            
+            // get the key for the selected row
+            let postID = postKey[indexPath.row]
+            
+            // remove the key and value from the array
+            postData.remove(at: indexPath.row)
+            postKey.remove(at: indexPath.row)
+            
+            // remove the value from Firebase
+            ref?.child("Posts").child("\(postID)").removeValue()
+            tableView.endUpdates()
+        }
+    }
+    
 }
 
